6b7297cfdf9460543b2d61e656d17ab820edbc4d
Saumya <saumyaharsh72@gmail.com>
2025-05-19 01:08:10 +0530

    new commit

--- questions/matrix.py
+++ questions/matrix.py
@@ -14,7 +14,7 @@ class matrix:
                 l1= []
                 ans = 0
                 for j in range(0,self.m1):
-                    ans  = self.a1[i][j] + b.a1[i][j]
+                    ans  = self.a1[i][j] + b.a1[j][i]
                     l1.append(ans)
                 ans_matrix.append(l1)
         else:
@@ -55,31 +55,76 @@ class matrix:
         ans = inv(self.a1)
         return ans
     
-n1 = int(input('Enter the no. of row of matrix1 '))
-m1 = int(input('Enter the no. of column of matrix 1 '))
-n2 = int(input('Enter the no. of row of matrix2 '))
-m2 = int(input('Enter the no. of column of matrix2 ' ))
-array1 = []
-array2 = []
-for i in range(0,n1):
-    l1 = []
-    for j in range(0,m1):
-        a = float(input('Enter elements of matrix1 '))
-        l1.append(a)
-    array1.append(l1)
-for i in range(0,n2):
-    l1 = []
-    for j in range(0,m2):
-        a = float(input('Enter elements of matrix2 '))
-        l1.append(a)
-    array2.append(l1)
+# n1 = int(input('Enter the no. of row of matrix1 '))
+# m1 = int(input('Enter the no. of column of matrix 1 '))
+# n2 = int(input('Enter the no. of row of matrix2 '))
+# m2 = int(input('Enter the no. of column of matrix2 ' ))
+# array1 = []
+# array2 = []
+# for i in range(0,n1):
+#     l1 = []
+#     for j in range(0,m1):
+#         a = float(input('Enter elements of matrix1 '))
+#         l1.append(a)
+#     array1.append(l1)
+# for i in range(0,n2):
+#     l1 = []
+#     for j in range(0,m2):
+#         a = float(input('Enter elements of matrix2 '))
+#         l1.append(a)
+#     array2.append(l1)
     
-m1 = matrix(array1,n1,m1)
-m2 = matrix(array2,n2,m2)
-ans = m1+m2
-ans2 = m1 - m2
-ans3 = m1*m2
-ans4 = m1.__inv__()
+# m1 = matrix(array1,n1,m1)
+# m2 = matrix(array2,n2,m2)
+# ans = m1+m2
+# ans2 = m1 - m2
+# ans3 = m1*m2
+# ans4 = m1.__inv__()
+# print(ans)
+# print(ans2)
+# print(ans3)
+# print(ans4)
+f = open('file1.txt','r')
+l1 = (f.read())
+
+
+array1 = []
+
+a = l1.split('\n')
+print(a)
+for line in a:
+    m = line.split(',')
+    l1 = []
+    for charc in m:
+        p = int(charc)
+        l1.append(p)
+    array1.append(l1)
+
+
+f = open('file2.txt','r')
+l12 = (f.read())
+array2 = []
+
+a2 = l12.split('\n')
+
+for line in a2:
+    m2 = line.split(',')
+    l2 = []
+    for charc in m2:
+        p = int(charc)
+        l2.append(p)
+    array2.append(l2)
+
+n1 = len(array1)
+m1 = len(array1[0])
+n2 = len(array2)
+m2 = len(array2[0])
+mat1 = matrix(array1,n1,m1)
+mat2= matrix(array2,n2,m2)
+ans = mat1+mat2
+ans2 = mat1 - mat2
+ans3 = mat1*mat2
+ans4 = mat1.__inv__()
 print(ans)
 print(ans2)
 print(ans3)
--- questions/tempCodeRunnerFile.py
+++ questions/tempCodeRunnerFile.py
@@ -1,2 +1,10 @@
-v6 = len(v1)
-# print (v6)
+m1 = matrix(array1,n1,m1)
+m2 = matrix(array2,n2,m2)
+ans = m1+m2
+ans2 = m1 - m2
+ans3 = m1*m2
+ans4 = m1.__inv__()
+print(ans)
+print(ans2)
+print(ans3)
+print(ans4)
